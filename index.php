2b5bb4490a1ef670cc75b62859ccf699f5425234
channarypha <73399453+channarypha@users.noreply.github.com>
2021-08-20 08:47:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Store management</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include_once('partial/header.php'); 
+include_once('partial/navbar.php'); 
+
+// get each page
+// echo $_GET['page'];
+if(isset($_GET['page'])) {
+    if ($_GET['page'] == 'home'){
+        include_once('pages/home.php');
+    }elseif($_GET['page'] == 'about'){
+        include_once('pages/about.php');
+    }elseif($_GET['page'] == 'customers'){
+        include_once('pages/customers.php');
+    }elseif($_GET['page'] == 'products_list'){
+        include_once('pages/products_list.php');
+    }elseif($_GET['page'] == 'orders'){
+        include_once('pages/orders.php');
+    }elseif($_GET['page'] == 'user_management'){
+        include_once('pages/user_management.php');
+    }elseif($_GET['page'] == 'contact'){
+        include_once('pages/contact.php');
+    }elseif($_GET['page'] == 'login'){
+        include_once('pages/login.php');
+    }elseif($_GET['page'] == 'register'){
+        include_once('pages/register.php');
+    }else{
+        include_once('pages/404.php');
+    }
+}else{
+       include_once('pages/home.php'); 
+}
+include_once('partial/footer.php'); 
